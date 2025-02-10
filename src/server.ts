133d2ceb7528564ced4baf9e9bf83850bae063a2
jpchavescruz <j.pvplayer0@gmail.com>
2025-02-10 14:35:38 -0300

    feat: configure express to recieve json type on body

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import express from 'express'
-import { getTasks, getTask } from './controllers/taskController'
+import express, { json } from 'express'
+import { getTasks, getTask, createTask } from './controllers/taskController'
 
 const app = express()
 const PORT = process.env.PORT || 3333
@@ -8,6 +8,7 @@ app.get('/test', (_, res) => {
   res.status(200).send({mensage: 'Hello World!'})
 })
 
+app.use(json())
 app.get('/tasks', getTasks)
 app.get('/tasks/:id', getTask)
 
