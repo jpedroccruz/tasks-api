88f1fe1d6cf564e0d89e47e34ffa6757282dce59
jpchavescruz <j.pvplayer0@gmail.com>
2025-02-10 15:18:24 -0300

    refact: change services function names

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -1,13 +1,13 @@
-import { createNewTask, updateTaskStatus, getAllTasks, getUniqueTask } from "../services/taskServices"
+import { create, update, getAll, getById } from "../services/taskServices"
 
 export async function getTasks(_: any, res: any) {
-  const tasks = await getAllTasks()
+  const tasks = await getAll()
   res.status(200).json(tasks)
 }
 
 export async function getTask(req: any, res: any) {
   const { id } = req.params
-  const task = await getUniqueTask(id)
+  const task = await getById(id)
   res.status(200).json(task)
 }
 
@@ -15,7 +15,7 @@ export async function createTask(req: any, res: any) {
   const { title, description } = req.body
   const created_at = (new Date).toLocaleDateString()
 
-  createNewTask(title, description, 'pending', created_at)
+  create(title, description, 'pending', created_at)
   res.status(201).json({ mensage: "Task created." })
 }
 
@@ -23,6 +23,6 @@ export async function updateTask(req: any, res: any) {
   const { status } = req.body
   const { id } = req.params
 
-  updateTaskStatus(id, status)
+  update(id, status)
   res.status(200).json({ mensage: "Task status updated."})
 }
