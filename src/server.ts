2eac53f76702bee565630b23b39a91d9f02ed57e
jpchavescruz <j.pvplayer0@gmail.com>
2025-02-10 16:29:29 -0300

    feat: seprate routes in routes.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express, { json } from 'express'
-import { getTasks, getTask, createTask, updateTask, deleteTask } from './controllers/taskController'
+import router from './routes'
 
 const app = express()
 const PORT = process.env.PORT || 3333
@@ -9,10 +9,6 @@ app.get('/test', (_, res) => {
 })
 
 app.use(json())
-app.get('/tasks', getTasks)
-app.get('/tasks/:id', getTask)
-app.post('/tasks', createTask)
-app.put('/tasks/:id', updateTask)
-app.delete('/tasks/:id', deleteTask)
+app.use(router)
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
