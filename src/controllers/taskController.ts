7eb9a8b46a4211bcf994d8cba40b7b73475f6972
jpchavescruz <j.pvplayer0@gmail.com>
2025-02-12 11:19:10 -0300

    fix: update req, res and next types

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -1,17 +1,18 @@
+import { Request, Response } from 'express'
 import { create, deleteById, getAll, getById, update } from "../services/taskServices"
 
-export async function getTasks(_: any, res: any) {
+export async function getTasks(_: Request, res: Response) {
   const tasks = await getAll()
   res.status(200).json(tasks)
 }
 
-export async function getTask(req: any, res: any) {
+export async function getTask(req: Request, res: Response) {
   const { id } = req.params
   const task = await getById(id)
   res.status(200).json(task)
 }
 
-export async function createTask(req: any, res: any) {
+export async function createTask(req: Request, res: Response) {
   const { title, description } = req.body
   const created_at = (new Date).toLocaleDateString()
 
@@ -19,7 +20,7 @@ export async function createTask(req: any, res: any) {
   res.status(201).json({ mensage: "Task created." })
 }
 
-export async function updateTask(req: any, res: any) {
+export async function updateTask(req: Request, res: Response) {
   const { status } = req.body
   const { id } = req.params
 
@@ -27,7 +28,7 @@ export async function updateTask(req: any, res: any) {
   res.status(200).json({ mensage: "Task status updated."})
 }
 
-export async function deleteTask(req: any, res: any) {
+export async function deleteTask(req: Request, res: Response) {
   const { id } = req.params
   deleteById(id)
   res.status(200).json({ mensage: "Task deleted." })
--- src/middlewares/taskMiddleware.ts
+++ src/middlewares/taskMiddleware.ts
@@ -1,26 +1,32 @@
-export function validateBody(req: any, res: any, next: any) {
+import { Request, Response, NextFunction } from "express"
+
+export function validateBody(req: Request, res: Response, next: NextFunction) {
   const { body } = req
 
   if (body.title === undefined || body.title === '') {
-    return res.status(400).json({ mensage: "Title field can't be undefined or empty."})
+    res.status(400).json({ mensage: "Title field can't be undefined or empty."})
+    return
   }
 
   if (body.description === undefined || body.description === '') {
-    return res.status(400).json({ mensage: "Descripntion field can't be undefined or empty."})
+    res.status(400).json({ mensage: "Descripntion field can't be undefined or empty."})
+    return
   }
 
   next()
 }
 
-export function validateStatus(req: any, res: any, next: any) {
+export function validateStatus(req: Request, res: Response, next: NextFunction) {
   const { body } = req
 
   if (body.status != 'pending' && body.status != 'completed') {
-    return res.status(400).json({ mensage: "Status field can only be pending or completed." })
+    res.status(400).json({ mensage: "Status field can only be pending or completed." })
+    return
   }
 
   if (body.status === undefined || body.status === '') {
-    return res.status(400).json({ mensage: "Status field can't be undefined or empty." })
+    res.status(400).json({ mensage: "Status field can't be undefined or empty." })
+    return
   }
 
   next()
