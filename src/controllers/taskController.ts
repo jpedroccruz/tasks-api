3d760e6658006e90594bd4d0d81fb0c317ff9f5c
jpchavescruz <j.pvplayer0@gmail.com>
2025-02-12 13:01:32 -0300

    feat: add validation on controllers

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -1,14 +1,21 @@
 import { Request, Response } from 'express'
 import { create, deleteById, getAll, getById, update } from "../services/taskServices"
+import BadRequest from '../middlewares/_error/badRequest'
 
 export async function getTasks(_: Request, res: Response) {
   const tasks = await getAll()
+
+  if (!tasks) throw new BadRequest('There are no tasks registered')
+
   res.status(200).json(tasks)
 }
 
 export async function getTask(req: Request, res: Response) {
   const { id } = req.params
   const task = await getById(parseInt(id))
+
+  if (!task) throw new BadRequest(`There are no tasks registred with id ${id}`)
+
   res.status(200).json(task)
 }
 
@@ -29,6 +36,6 @@ export async function updateTask(req: Request, res: Response) {
 
 export async function deleteTask(req: Request, res: Response) {
   const { id } = req.params
-  deleteById(parseInt(id))
+  deleteById(parseInt(id)) 
   res.status(200).json({ mensage: "Task deleted." })
 }
