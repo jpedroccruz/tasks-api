940bb97598bd65a0de5a406d13da3fc98a748de0
jpchavescruz <j.pvplayer0@gmail.com>
2025-02-12 14:03:31 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,15 @@ import express, { json } from 'express'
 import tasksRoute from './routes/taskRoute'
 import swagerRoute from './routes/swagerRoute'
 import errorHandler from './middlewares/errorHandler'
+import cors from 'cors'
 
 const app = express()
 const PORT = process.env.PORT || 3333
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}))
 
 app.get('/test', (_, res) => {
   res.status(200).send({mensage: 'Hello World!'})
