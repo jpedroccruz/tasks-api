bd248f7db947f0f1569ad0816fc0d21b9d6ca7ca
jpchavescruz <j.pvplayer0@gmail.com>
2025-02-12 12:03:56 -0300

    refact: separate validateBory middleware

--- src/middlewares/taskMiddleware.ts
+++ src/middlewares/taskMiddleware.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express"
 
-export function validateBody(req: Request, res: Response, next: NextFunction) {
+export function validateTitle(req: Request, res: Response, next: NextFunction) {
   const { body } = req
 
   if (body.title === undefined || body.title === '') {
@@ -8,8 +8,14 @@ export function validateBody(req: Request, res: Response, next: NextFunction) {
     return
   }
 
+  next()
+}
+
+export function validateDescription(req: Request, res: Response, next: NextFunction) {
+  const { body } = req
+
   if (body.description === undefined || body.description === '') {
-    res.status(400).json({ mensage: "Descripntion field can't be undefined or empty."})
+    res.status(400).json({ mensage: "Description field can't be undefined or empty."})
     return
   }
 
@@ -19,13 +25,13 @@ export function validateBody(req: Request, res: Response, next: NextFunction) {
 export function validateStatus(req: Request, res: Response, next: NextFunction) {
   const { body } = req
 
-  if (body.status != 'pending' && body.status != 'completed') {
-    res.status(400).json({ mensage: "Status field can only be pending or completed." })
+  if (body.status === undefined || body.status === '') {
+    res.status(400).json({ mensage: "Status field can't be undefined or empty." })
     return
   }
 
-  if (body.status === undefined || body.status === '') {
-    res.status(400).json({ mensage: "Status field can't be undefined or empty." })
+  if (body.status != 'pending' && body.status != 'completed') {
+    res.status(400).json({ mensage: "Status field can only be 'pending' or 'completed'." })
     return
   }
 
