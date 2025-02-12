34b235bd8224c38a49ade7829eaadbb6ff6e6058
jpchavescruz <j.pvplayer0@gmail.com>
2025-02-12 13:37:24 -0300

    refact: remove unnecessary validation

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -1,21 +1,14 @@
 import { Request, Response } from 'express'
 import { create, deleteById, getAll, getById, update } from "../services/taskServices"
-import BadRequest from '../middlewares/_error/badRequest'
 
 export async function getTasks(_: Request, res: Response) {
   const tasks = await getAll()
-
-  if (!tasks) throw new BadRequest('There are no tasks registered')
-
   res.status(200).json(tasks)
 }
 
 export async function getTask(req: Request, res: Response) {
   const { id } = req.params
   const task = await getById(parseInt(id))
-
-  if (!task) throw new BadRequest(`There are no tasks registred with id ${id}`)
-
   res.status(200).json(task)
 }
 
